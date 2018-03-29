d68dc7857deca8d747648588935bb1838ba600a6
baixiangzhu <baixiangzhu@gomeplus.com>
2018-03-29 14:22:47 +0800

    测试文件

---
+++ src/main/java/com/example/demo/TestDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@ComponentScan("com.example.demo")
+@SpringBootApplication
+public class TestDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDockerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author baixiangzhu
+ * @date 2018/3/29
+ **/
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
